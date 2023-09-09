84552748c7c367d2d8094f1db0ae0b661ad3349e
Zirui Liu <80185375+zero-8848@users.noreply.github.com>
2023-09-09 12:45:18 +1000

    Delete fit2099 directory

--- fit2099/2099assignment/engine/positions/GroundFactory.java
+++
@@ -1,8 +0,0 @@
-package edu.monash.fit2099.engine.positions;
-
-/**
- * Interface for factory classes used by GameMap to create new map locations.
- */
-public interface GroundFactory {
-	Ground newGround(char displayChar);
-}
--- fit2099/2099assignment/game/time/TimePeriod.java
+++
@@ -1,9 +0,0 @@
-package game.time;
-
-/**
- * day and night
- */
-public enum TimePeriod {
-    DAY,//day time period
-    NIGHT//night time period
-}
